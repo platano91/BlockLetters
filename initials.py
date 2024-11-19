d54e754676d2d8b47fc6c7bfe5fd94a3374fed98
Samuel <salvadomo@hotmail.com>
2024-11-19 10:21:03 -0700

    complete

---
+++ initials.py
@@ -0,0 +1,9 @@
+# Sam Delacruz
+# I have heterochromia iridim
+print("  SSS   DDDD  ")
+print(" S   S  D   D ")
+print(" S      D   D ")
+print("  SSS   D   D ")
+print("     S  D   D ")
+print(" S   S  D   D ")
+print("  SSS   DDDD  ")
